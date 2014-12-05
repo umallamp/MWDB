525fb715cd1047ed3992fe1fb8a98a81a01a7cf1
Sampath Mallampalli <umallamp@asu.edu>
2014-12-04 23:19:21 -0700

    Changed the notation of b bits for each dimensions

--- Phase3/Code/getIndexStructure.m
+++ Phase3/Code/getIndexStructure.m
@@ -13,7 +13,7 @@ dataMatrix = getNormalizedMatrix(dataMatrix);
 [ reducedDataMatrix, reCreateMatrix ] = getReducedMatrix( dataMatrix );
 
 % size of reduced data matrix
-[rowCount, colCount] = size(reducedDataMatrix);
+[rowCount, totalDims] = size(reducedDataMatrix);
 
 % calculate the min and max boundaries
 minValues = min(reducedDataMatrix);
@@ -22,19 +22,19 @@ maxValues = max(reducedDataMatrix);
 % maxValues = ones(colCount);
 
 % array for bits per dimension
-bitsPerDims = zeros(1, colCount);
+bitsPerDims = ones(1, totalDims) * bitsPerVector;
 
-% compute the number of bits for each dimension
-remainder = mod(bitsPerVector, colCount);
-bitsPerDim = floor(bitsPerVector / colCount);
-
-for index = 1 : colCount
-    if(index <= remainder)
-        bitsPerDims(index) = bitsPerDim + 1;
-    else
-        bitsPerDims(index) = bitsPerDim;
-    end
-end
+% % compute the number of bits for each dimension
+% remainder = mod(bitsPerVector, colCount);
+% bitsPerDim = floor(bitsPerVector / colCount);
+% 
+% for index = 1 : colCount
+%     if(index <= remainder)
+%         bitsPerDims(index) = bitsPerDim + 1;
+%     else
+%         bitsPerDims(index) = bitsPerDim;
+%     end
+% end
 
 % compute the vector approximation for each file and add to vector
 % approximiation index
@@ -46,15 +46,15 @@ fileApproxIndex = [];
 
 for fileId = 1 : rowCount
     fileApprox = '';
-    for dimId = 1 : colCount
+    for dimId = 1 : totalDims
         fileApprox = strcat(fileApprox, dec2bin(dataSetRegions(fileId, dimId), bitsPerDims(dimId)));
     end
     fileApproxIndex = [fileApproxIndex; fileApprox]; 
 end
 
 % compute the size of index file
-[rowSize, colSize] = size(fileApproxIndex);
-fileSize = (rowSize * colSize) / 8;
+[rowSize, ~] = size(fileApproxIndex);
+fileSize = (rowSize * totalDims * bitsPerVector) / 8;
 
 end
 
